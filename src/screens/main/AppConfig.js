685ffee999d65f08ee6657ba26c2244bd192d938
HeeYeonKim98 <wow9896@naver.com>
2020-01-07 20:23:08 +0900

    HYcommit1

---
+++ src/screens/main/AppConfig.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text} from "react-native";
+
+export default class AppConfig extends Component {
+  render() {
+    return (
+      <View>
+          <Text>
+              앱정보 화면
+          </Text>
+      </View>
+    );
+  }
+}
