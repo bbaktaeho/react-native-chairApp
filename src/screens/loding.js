6973d4f9750f1e8f28953c8795b312d9984d7edb
BbakTaeHo <lth9036@gmail.com>
2020-01-09 14:42:30 +0900

    revised_loding

--- src/screens/loding.js
+++ src/screens/loding.js
@@ -27,6 +27,7 @@ export default class Loading extends Component {
       <ImageBackground
         source={require("../assets/Images/chair_logo.png")}
         style={{ width: "100%", height: "100%" }}
+        resizeMode="contain"
       ></ImageBackground>
     );
   }
