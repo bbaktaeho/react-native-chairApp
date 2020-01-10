22c6182de059b5ca21dd4e245cc74de3937517de
BbakTaeHo <lth9036@gmail.com>
2020-01-10 18:04:41 +0900

    revised_homeTwo_1

--- src/screens/main/Home_one.js
+++ src/screens/main/Home_one.js
@@ -1,54 +1,63 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet,ToastAndroid, BackHandler } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ToastAndroid,
+  BackHandler,
+  ScrollView,
+  FlatList
+} from "react-native";
+import { Card } from "react-native-elements";
 import MyHeader from "../../components/MyHeader";
 
 export default class Home_one extends Component {
+  constructor(props) {
+    super(props);
+    this.handleBackButton = this.handleBackButton.bind(this);
+    this.state = {};
+  }
+  // 이벤트 등록
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  // 이벤트 해제
+  componentWillUnmount() {
+    this.exitApp = false;
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  // 이벤트 동작
+  handleBackButton = () => {
+    // 2000(2초) 안에 back 버튼을 한번 더 클릭 할 경우 앱 종료
+    if (this.exitApp == undefined || !this.exitApp) {
+      ToastAndroid.show("한번 더 누르시면 종료됩니다.", ToastAndroid.SHORT);
+      this.exitApp = true;
+
+      this.timeout = setTimeout(
+        () => {
+          this.exitApp = false;
+        },
+        2000 // 2초
+      );
+    } else {
+      clearTimeout(this.timeout);
+      BackHandler.exitApp(); // 앱 종료
+    }
+    return true;
+  };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MyHeader navigation={this.props.navigation} title="홈 1"></MyHeader>
-        <View style={styles.container}>
-          <Text>홈1</Text>
-        </View>
+        <ScrollView>
+          <View style={styles.container}></View>
+        </ScrollView>
       </View>
     );
   }
-  constructor(props) {
-    super(props)
-    this.handleBackButton= this.handleBackButton.bind(this);
-}
-   // 이벤트 등록
-   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
-}
-
-// 이벤트 해제
-componentWillUnmount() {
-    this.exitApp = false;
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
-}
-
-// 이벤트 동작
-handleBackButton = () => {
-    // 2000(2초) 안에 back 버튼을 한번 더 클릭 할 경우 앱 종료
-    if (this.exitApp == undefined || !this.exitApp) {
-        ToastAndroid.show('한번 더 누르시면 종료됩니다.', ToastAndroid.SHORT);
-        this.exitApp = true;
-
-        this.timeout = setTimeout(
-            () => {
-                this.exitApp = false;
-            },
-            2000    // 2초
-        );
-    } else {
-        clearTimeout(this.timeout);
-
-        BackHandler.exitApp();  // 앱 종료
-    }
-    return true;
-}
-
 }
 
 const styles = StyleSheet.create({
--- src/screens/main/Home_two.js
+++ src/screens/main/Home_two.js
@@ -1,15 +1,214 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, FlatList } from "react-native";
+import { Card } from "react-native-elements";
 import MyHeader from "../../components/MyHeader";
 
 export default class Home_two extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backData: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+      seatData: [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 },
+        { id: 10 },
+        { id: 11 },
+        { id: 12 },
+        { id: 13 },
+        { id: 14 },
+        { id: 15 },
+        { id: 16 },
+        { id: 17 },
+        { id: 18 },
+        { id: 19 },
+        { id: 20 },
+        { id: 21 },
+        { id: 22 },
+        { id: 23 },
+        { id: 24 },
+        { id: 25 },
+        { id: 26 },
+        { id: 27 },
+        { id: 28 },
+        { id: 29 },
+        { id: 30 },
+        { id: 31 }
+      ]
+    };
+  }
+  // 이벤트 등록
+  componentDidMount() {}
+
+  // 이벤트 해제
+  componentWillUnmount() {}
+
+  _renderItem = ({ item }) => {
+    return <Card containerStyle={{ flex: 1 }}></Card>;
+  };
+
   render() {
+    const { backData, seatData } = this.state;
     return (
       <View style={{ flex: 1 }}>
-        <MyHeader navigation={this.props.navigation} title="홈 2"></MyHeader>
-        <View style={styles.container}>
-          <Text>홈2</Text>
-        </View>
+        <MyHeader navigation={this.props.navigation} title="센서"></MyHeader>
+        <ScrollView>
+          <View style={styles.container}>
+            <Card title="등받이" containerStyle={{ width: "100%", flex: 1 }}>
+              {/* <FlatList
+                data={backData}
+                horizontal={true}
+                renderItem={(item, index) => this._renderItem(item)}
+                keyExtractor={item => item.id.toString()} // 각 아이템의 키 값을 지정
+              /> */}
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  marginBottom: 8
+                }}
+              >
+                {backData
+                  .filter(e => e.id <= 2)
+                  .map(e => (
+                    <View
+                      style={{
+                        width: "45%",
+                        backgroundColor: "white",
+                        height: 100,
+                        borderWidth: 1,
+                        borderColor: "black"
+                      }}
+                      key={e.id}
+                    />
+                  ))}
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around"
+                }}
+              >
+                {backData
+                  .filter(e => e.id > 2)
+                  .map(e => (
+                    <View
+                      style={{
+                        width: "45%",
+                        backgroundColor: "white",
+                        height: 100,
+                        borderWidth: 1,
+                        borderColor: "black"
+                      }}
+                      key={e.id}
+                    />
+                  ))}
+              </View>
+            </Card>
+            <Card title="엉덩받이" containerStyle={{ width: "100%", flex: 1 }}>
+              <Text>다리쪽</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  marginBottom: 8
+                }}
+              >
+                {seatData
+                  .filter(e => e.id <= 6)
+                  .map(e => (
+                    <View
+                      style={{
+                        width: "14%",
+                        height: 40,
+                        backgroundColor: "white",
+                        borderWidth: 1,
+                        borderColor: "black"
+                      }}
+                      key={e.id}
+                    />
+                  ))}
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  marginBottom: 8
+                }}
+              >
+                {this.state.seatData
+                  .filter(e => e.id > 6 && e.id <= 20)
+                  .map(e => (
+                    <View
+                      style={{
+                        width: "5%",
+                        height: 40,
+                        backgroundColor: "white",
+                        borderWidth: 1,
+                        borderColor: "black"
+                      }}
+                      key={e.id}
+                    />
+                  ))}
+              </View>
+              <View
+                style={{ flexDirection: "row", justifyContent: "space-around" }}
+              >
+                {this.state.seatData
+                  .filter(e => e.id > 20)
+                  .map(e => (
+                    <View
+                      style={{
+                        width: "7%",
+                        height: 40,
+                        backgroundColor: "white",
+                        borderWidth: 1,
+                        borderColor: "black"
+                      }}
+                      key={e.id}
+                    />
+                  ))}
+              </View>
+              <Text>엉덩이쪽</Text>
+              {/* <FlatList
+                data={seatData.filter(e => e.id <= 10)}
+                ListFooterComponentStyle={{
+                  flex: 1,
+                  justifyContent: "space-around"
+                }}
+                horizontal={true}
+                renderItem={(item, index) => this._renderItem(item)}
+                keyExtractor={item => item.id.toString()} // 각 아이템의 키 값을 지정
+              />
+              <FlatList
+                data={seatData.filter(e => e.id > 10 && e.id <= 25)}
+                ListFooterComponentStyle={{
+                  flex: 1,
+                  justifyContent: "space-around"
+                }}
+                horizontal={true}
+                renderItem={(item, index) => this._renderItem(item)}
+                keyExtractor={item => item.id.toString()} // 각 아이템의 키 값을 지정
+              />
+              <FlatList
+                data={seatData.filter(e => e.id > 25)}
+                ListFooterComponentStyle={{
+                  flex: 1,
+                  justifyContent: "space-around"
+                }}
+                horizontal={true}
+                renderItem={(item, index) => this._renderItem(item)}
+                keyExtractor={item => item.id.toString()} // 각 아이템의 키 값을 지정
+              /> */}
+            </Card>
+          </View>
+        </ScrollView>
       </View>
     );
   }
