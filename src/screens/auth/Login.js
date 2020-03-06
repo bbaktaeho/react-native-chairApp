fd0e6eef6b26eb6cdd9c7bb2851d8393756b231e
bbaktaeho <lth9036@gmail.com>
2020-03-07 02:04:50 +0900

    마지막 테스트..

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -123,7 +123,7 @@ class Login extends React.Component {
           )}
           {this.state.email === "" && this.state.passwd === "" && (
             <AuthButton
-              title="비회원"
+              title="비회원으로 시작하기"
               backColor="#CEAEA7"
               onPress={() => this.props.navigation.navigate("Guide")}
             ></AuthButton>
