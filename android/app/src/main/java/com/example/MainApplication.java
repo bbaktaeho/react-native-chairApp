1a31b033508b67a3c7bc66b804a936f637aaffdd
bbaktaeho <lth9036@gmail.com>
2020-04-15 01:17:33 +0900

    태호가 로컬알림 라이브러리 적용에 성공했습니다

--- android/app/src/main/java/com/example/MainApplication.java
+++ android/app/src/main/java/com/example/MainApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
