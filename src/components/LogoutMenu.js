01055d0419ccdf92a47e32f7fb163f60484b5ca9
bbaktaeho <lth9036@gmail.com>
2020-05-20 22:45:18 +0900

    에러 수정

--- src/components/LogoutMenu.js
+++ src/components/LogoutMenu.js
@@ -2,30 +2,32 @@ import React from "react";
 import { Alert, BackHandler, AsyncStorage } from "react-native";
 import { Icon } from "react-native-elements";
 import { connect } from "react-redux";
-import URL from '../NET'
-import Fetch from '../components/Fetch'
+import URL from "../NET";
+import Fetch from "../components/Fetch";
 
 const LogoutMenu = (props) => {
-
   exit_removeItem = async () => {
     try {
-      const token = await AsyncStorage.getItem('token')
-      const {data1, data2, data3, data4, data5, data6} = props.statData
+      const token = await AsyncStorage.getItem("token");
+      const { data1, data2, data3, data4, data5, data6 } = props.statData;
       let posarr = [data1, data2, data3, data4, data5, data6];
-      const result = await Fetch(URL.posturesave, 'POST', {
-        postures: posarr
-      }, token)
-      const body = JSON.parse(result._bodyText)
-      if (body.success) console.log('저장 성공');
+      const result = await Fetch(
+        URL.posturesave,
+        "POST",
+        {
+          postures: posarr,
+        },
+        token
+      );
+      const body = JSON.parse(result._bodyText);
+      if (body.success) console.log("저장 성공");
       else {
-        console.log('저장 실패');
-        await AsyncStorage.setItem("tmpPostures", JSON.stringify(posarr))
+        console.log("저장 실패");
+        await AsyncStorage.setItem("tmpPostures", JSON.stringify(posarr));
       }
       await AsyncStorage.removeItem("token");
-    BackHandler.exitApp();
-    } catch (err) {
-
-    }
+      BackHandler.exitApp();
+    } catch (err) {}
   };
 
   return (
@@ -40,7 +42,8 @@ const LogoutMenu = (props) => {
           [
             { text: "취소", style: "cancel" },
             {
-              text: "종료", style='destructive',
+              text: "종료",
+              style: "destructive",
               onPress: () => this.exit_removeItem(),
             },
           ],
