baaef9adc71e840efe68ac7ff345b17531560837
bbaktaeho <lth9036@gmail.com>
2020-06-18 22:50:28 +0900

    Complete: Chair App

--- node_modules/@react-native-community/push-notification-ios/index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import {App} from './example/App';
-import {name as appName} from './example/app.json';
-
-AppRegistry.registerComponent(appName, () => App);
